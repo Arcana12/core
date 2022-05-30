5b3031c4d534b74eb390181ae0af0b2b66b3cbe4
Arcana12 <arcdrive12@gmail.com>
2022-05-30 13:50:03 +0900

    Bean Lifecycle Callback @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,10 @@
 package hello.core.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.beans.ConstructorProperties;
+
 public class NetworkClient {
 
 
@@ -32,12 +36,14 @@ public class NetworkClient {
     }
 
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
